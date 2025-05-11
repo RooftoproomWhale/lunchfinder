ab1ff4122b50fd53f910dfcfe156cbf113d08bf9
RooftoproomWhale <gh8238@naver.com>
2025-05-12 06:51:58 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/main/java/com/woong/lunchfinder/LunchfinderApplication.java
@@ -0,0 +1,13 @@
+package com.woong.lunchfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchfinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunchfinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woong/lunchfinder/LunchfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woong.lunchfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchfinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
